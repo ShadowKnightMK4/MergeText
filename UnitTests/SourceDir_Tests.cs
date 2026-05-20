a3accdd79e5dd36d5c8295bb4d18623f1a9f2283
Thomas Paul Betterly <thomasbetterly@aol.com>
2026-05-20 08:28:40 -0400

    random update

--- UnitTests/SourceDir_Tests.cs
+++ UnitTests/SourceDir_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using MergeText;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
--- UnitTests/WildcardMatch_Tests.cs
+++ UnitTests/WildcardMatch_Tests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MergeText;
 using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTests
 {
     [TestClass]
